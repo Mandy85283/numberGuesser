9eb1887ac7dfc80ed3fca0468bfbee8175ea2832
Mandy85283 <Mandeep85283@gmail.com>
2022-04-16 16:10:20 -0400

    Project complete

--- numberGuesser/ViewController.swift
+++ numberGuesser/ViewController.swift
@@ -9,9 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var guessLabel: UILabel!
+    @IBOutlet weak var guessSlider: UISlider!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func showAnswer(_ sender: Any) {
+        let maxVal = Int(guessSlider.maximumValue)
+        let answerVal = Int.random(in: 0...maxVal)
+        let guess = Int(guessSlider.value)
+        
+        answerLabel.text = String(answerVal)
+        UIView.animate(withDuration: 0.5) {
+            () -> Void in
+            self.resultLabel.alpha = 1
+            if(answerVal == guess){
+                self.resultLabel.text = NSLocalizedString("congratulations", comment: "You win")
+        } else {
+            self.resultLabel.text = NSLocalizedString("better luck next time", comment: "You loss")
+        }
+        }
+        UIView.animate(withDuration: 2) {
+            () -> Void in
+            self.resultLabel.alpha = 0
+            
+        }
+            if(answerVal == guess){
+                self.view.backgroundColor = .green
+            }else{
+                self.view.backgroundColor = .red
+                
+            }
+        }
+    
+    
+    @IBAction func setGuess(_ sender: UISlider) {
+        guessLabel.text = String(Int(sender.value))
+    }
+    
+    @IBAction func changeDiff(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            guessSlider.tintColor = .blue
+            guessSlider.maximumValue = 10
+        case 1:
+            guessSlider.tintColor = .yellow
+            guessSlider.maximumValue = 100
+        case 2:
+            guessSlider.tintColor = .purple
+            guessSlider.maximumValue = 1000
+        default:
+            guessSlider.tintColor = .blue
+            guessSlider.maximumValue = 10
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        resultLabel.text = ""
+
     }
 
 
